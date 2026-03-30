eb0279e5223b1c064bf8b437c9f81e5c3547f02f
DuyAlin-99 <veresbaby0108@gmail.com>
2026-03-31 02:15:29 +0700

    Fix db config

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,12 @@ const mysql = require('mysql2');
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  password: process.env.DB_PASS, // Sửa từ DB_PASSWORD thành DB_PASS
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 16267, // Thêm dòng này để nhận cổng 16267
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = db.promise();
