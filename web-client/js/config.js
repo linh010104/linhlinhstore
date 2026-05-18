1efa78afc513d8e1cfc2e6511911d7fb8c3bb1e3
DuyAlin-99 <veresbaby0108@gmail.com>
2026-05-18 10:12:20 +0700

    linhlinh public 18/5

--- web-client/js/config.js
+++ web-client/js/config.js
@@ -1,5 +1,11 @@
+// const CONFIG = {
+//     BASE_URL: "http://localhost:3000/api",
+//     IMAGE_BASE_URL: "http://localhost:3000"
+// };
+// window.CONFIG = CONFIG;
+
 const CONFIG = {
-    BASE_URL: "http://localhost:3000/api",
-    IMAGE_BASE_URL: "http://localhost:3000"
+    BASE_URL: "/api",
+    IMAGE_BASE_URL: ""
 };
 window.CONFIG = CONFIG;
