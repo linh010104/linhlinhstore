c1d21c37c7201da9fd76cd2fc0c1621fac9224a4
DuyAlin-99 <veresbaby0108@gmail.com>
2026-05-18 10:28:12 +0700

    linhlinh public 18/5

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 
-const db = mysql.createPool({
-    // Lấy thông tin từ mây (nếu có), không thì tự dùng localhost của máy tính
+// 1. Tạo Pool kết nối tiêu chuẩn (Dành cho 7 file Models chạy Callback)
+const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'dientu_store',
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 100,
     queueLimit: 0
 });
 
-// Xuất ra dạng promise để API dùng được async/await
-module.exports = db.promise();
+const promisePool = pool.promise();
+pool.promise = () => promisePool;
+module.exports = pool;
 
 // const mysql = require('mysql2');
 
